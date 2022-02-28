3a3edc5fd591728ca903189eef9c96d9c07974dc
LENOVO <hegdenagashree19@gmail.com>
2022-02-28 00:09:59 -0800

    heyyyy

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
 
-    path('', views.index,name="index")
-    
+    path('', views.index,name="list"),
+    path('updatetask/<str:pk>/', views.updatetask,name="updatetask"),
+    path('deletetask/<str:pk>/', views.deletetask,name="deletetask"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from . models import *
+
+from .forms import *
 
 # Create your views here.
 def index(request):
    tasks=Task.objects.all()
-   contest = {'tasks':tasks}
+
+   form=TaskForm()
+
+   if request.method=="POST":
+      form=TaskForm(request.POST)
+      if form.is_valid():
+         form.save()
+      return redirect('/') 
+   contest = {'tasks':tasks,'form':form}
    return render(request,'tasks/list.html',contest) 
+
+
+def updatetask(request,pk):
+   task=Task.objects.get(id=pk)
+
+   form=TaskForm(instance=task)
+
+   if request.method=="POST":
+      form=TaskForm(request.POST,instance=task)
+      if form.is_valid():
+         form.save()
+      return redirect('/')   
+
+   contest={'form':form}
+
+   return render(request,'tasks/updatetask.html',contest)   
+
+
+def deletetask(request,pk):
+   item=Task.objects.get(id=pk)
+   if request.method=="POST":
+      item.delete()
+      return redirect('/') 
+
+   contest={'item':item}
+   return render(request,'tasks/delete.html',contest)
